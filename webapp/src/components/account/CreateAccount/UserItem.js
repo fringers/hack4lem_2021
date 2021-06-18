bd3acd9d362492609d1f7b4640f61400bcfe83a7
Jakub Król <jakub.krol@mc2innovations.com>
2021-06-19 01:19:38 +0200

    users list item

---
+++ webapp/src/components/account/CreateAccount/UserItem.js
@@ -0,0 +1,58 @@
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+
+export const UserItem = () => {
+  return (
+    <Paper variant="outlined">
+      <Box px={3} py={2}>
+        <Grid container direction="column" spacing={1}>
+          <Grid item>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+            >
+              <Grid item>Jan Kowalski (Ty)</Grid>
+
+              <Grid item>
+                <IconButton size="small">
+                  <DeleteIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item>
+            <FormControlLabel
+              control={<Checkbox name="test" />}
+              label="Zezwalaj na wypłaty"
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              id="budget"
+              label="Wkład"
+              type="number"
+              min="0"
+              max="100"
+              variant="outlined"
+              placeholder="np. 30"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">%</InputAdornment>,
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </Paper>
+  );
+};
--- webapp/src/components/account/CreateAccount/UsersList.js
+++ webapp/src/components/account/CreateAccount/UsersList.js
@@ -1,20 +1,13 @@
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
+import { UserItem } from "./UserItem";
 
 export const UsersList = () => {
   return (
     <Grid container direction="column" spacing={2}>
       <Grid item>
-        <Typography variant="subtitle1"> Osoby z dostępem do konta</Typography>
+        <Typography variant="subtitle1">Osoby z dostępem do konta</Typography>
       </Grid>
 
       <Grid item>
@@ -24,22 +17,7 @@ export const UsersList = () => {
       </Grid>
 
       <Grid item>
-        <List>
-          <ListItem>
-            <ListItemText primary="Ty" />
-            <ListItemIcon>
-              <Checkbox edge="start" tabIndex={-1} disableRippl />
-            </ListItemIcon>
-            <ListItemIcon>
-              <Checkbox edge="start" tabIndex={-1} disableRippl />
-            </ListItemIcon>
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="comments">
-                <DeleteIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        </List>
+        <UserItem />
       </Grid>
     </Grid>
   );
