cfab6cdbf3b9763258b4a4b396262f018fd8a40d
robi24 <robi240@gmail.com>
2021-06-19 22:59:43 +0100

    feat: typo

--- webapp/src/components/NotImplemented/index.js
+++ webapp/src/components/NotImplemented/index.js
@@ -10,7 +10,7 @@ export const NotImplemented = ({ open, onClose }) => {
         <p>
           Czas był ograniczony i nie byliśmy w stanie zaimplmentować wszystkich
           przepływów w aplikacji. Dlatego możesz trafiać na komunikaty jak ten.
-          Koniecznie zajrzyj do nasze prezentacji - znajdziesz tam opisy każdej
+          Koniecznie zajrzyj do naszej prezentacji - znajdziesz tam opisy każdej
           z funkcjonalności, której nie zdążyliśmy tu pokazać!
         </p>
       </Box>
