680664d0a36aa79b708b13d584fe1f012c088795
Jakub Król <jakub.krol@mc2innovations.com>
2021-06-19 10:55:12 +0200

    users

---
+++ webapp/src/data/users.js
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    id: 1,
+    email: "ali.connors@gmail.com",
+    fullName: "Ali Connors",
+    avatar: "https://material-ui.com/static/images/avatar/1.jpg",
+  },
+  {
+    id: 2,
+    email: "travis.howard@gmail.com",
+    fullName: "Travis Howard",
+    avatar: "https://material-ui.com/static/images/avatar/2.jpg",
+  },
+  {
+    id: 3,
+    email: "sandra.adams@gmail.com",
+    fullName: "Sandra Adams",
+    avatar: "https://material-ui.com/static/images/avatar/3.jpg",
+  },
+];
+
+export const getUserById = (id) => users.find((u) => u.id == id);
