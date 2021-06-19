4fcb934538f2cd98c861f4e7df61b4cbfc0fff45
Jakub Król <jakub.krol@mc2innovations.com>
2021-06-19 10:52:07 +0200

    refactor

--- webapp/src/components/dashboard/BudgetPreviewItem.js
+++ webapp/src/components/dashboard/BudgetPreviewItem.js
@@ -2,15 +2,13 @@ import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import { UserAvatar } from "../avatar/UserAvatar";
 
 const useStyles = makeStyles((theme) => ({
-  name: {
-    fontWeight: "bold",
-  },
   textSeparator: {
     paddingLeft: "8px",
     paddingRight: "8px",
@@ -22,6 +20,10 @@ const useStyles = makeStyles((theme) => ({
     height: 25,
     borderRadius: 6,
   },
+  progressText: {
+    color: "white",
+    fontWeight: "medium",
+  },
 }));
 
 export const BudgetPreviewItem = ({ id, name, budget, available }) => {
@@ -29,70 +31,57 @@ export const BudgetPreviewItem = ({ id, name, budget, available }) => {
 
   const percent = (available * 100) / budget;
 
-  const ProgressText = ({ className }) => (
-    <span className={`text-sm font-semibold pl-2 ${className}`}>
-      {available}/{budget} PLN
-    </span>
-  );
-
   return (
     <Card>
-      <CardContent>
-        <div>
-          <Typography
-            component={Link}
-            display="inline"
-            variant="subtitle1"
-            className={classes.name}
-            to={`/accounts/details/${id}`}
-          >
-            {name}
-          </Typography>
-          <Typography display="inline" className={classes.textSeparator}>
-            |
-          </Typography>
-          <Typography display="inline">Odnawiane co miesiąc</Typography>
-        </div>
-
-        <Box position="relative" className={classes.progressBoxWrapper}>
-          <Box position="absolute" left="0" right="0" top="0" bottom="0">
-            <LinearProgress
-              variant="determinate"
-              value={percent}
-              className={classes.progressBar}
-            />
+      <CardActionArea component={Link} to={`/accounts/details/${id}`}>
+        <CardContent>
+          <Box>
+            <Typography display="inline" variant="body2">
+              Restauracje
+            </Typography>
+            <Typography
+              display="inline"
+              variant="body2"
+              className={classes.textSeparator}
+            >
+              |
+            </Typography>
+            <Typography display="inline" variant="body2">
+              Odnawiane co miesiąc
+            </Typography>
           </Box>
-          <Box position="absolute">text</Box>
-        </Box>
 
-        <div className="relative border rounded bg-blue-200 h-8 mt-2">
-          <div className="absolute h-full">
-            <ProgressText className="text-black" />
-          </div>
+          <Box>
+            <Typography variant="h6">{name}</Typography>
+          </Box>
 
-          <div
-            className="absolute h-full rounded bg-primary-light overflow-clip overflow-hidden"
-            style={{ width: `${percent}%` }}
-          >
-            <ProgressText className="text-white" />
-          </div>
+          <Box py={1}>
+            <Box position="relative" className={classes.progressBoxWrapper}>
+              <Box position="absolute" left="0" right="0" top="0" bottom="0">
+                <LinearProgress
+                  variant="determinate"
+                  value={percent}
+                  className={classes.progressBar}
+                />
+              </Box>
 
-          <div className="absolute right-1 top-1 flex flex-row -space-x-1">
-            <UserAvatar
-              small
-              src="https://randomuser.me/api/portraits/women/13.jpg"
-            />
-            <UserAvatar
-              small
-              src="https://randomuser.me/api/portraits/men/90.jpg"
-            />
-            <UserAvatar
-              small
-              src="https://randomuser.me/api/portraits/lego/1.jpg"
-            />
-          </div>
-        </div>
-      </CardContent>
+              <Box position="absolute" pl={1}>
+                <Typography variant="body1" className={classes.progressText}>
+                  {available}/{budget} PLN
+                </Typography>
+              </Box>
+
+              <Box position="absolute" right="0" top="0" bottom="0">
+                <Box pr={1}>
+                  <UserAvatar />
+                  <UserAvatar />
+                  <UserAvatar />
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
